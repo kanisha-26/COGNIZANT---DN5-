586f0e37059b38447c63ab9d8ab240ff374c6aa5
KANISHA M D <kanishamd26@gmail.com>
2026-06-25 13:31:00 +0530

    Add files via upload

---
+++ Frontend Development/hands-on-7/student-profile.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-student-profile',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './student-profile.html',
+  styleUrl: './student-profile.css'
+})
+export class StudentProfile {
+
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    semester: new FormControl('', [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(8)
+    ])
+  });
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+  }
+}
