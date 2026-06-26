8e0f5fab84e7fa43687d21022e68e9215bfcff66
KANISHA M D <kanishamd26@gmail.com>
2026-06-26 21:10:11 +0530

    Add files via upload

---
+++ Frontend Development/hands-on-10/src/components/CourseCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <h3>{{ name }}</h3>
+    <p><strong>Code:</strong> {{ code }}</p>
+    <p><strong>Credits:</strong> {{ credits }}</p>
+    <p><strong>Grade:</strong> {{ grade }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: String,
+  code: String,
+  credits: Number,
+  grade: String
+})
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  padding: 15px;
+  margin: 10px 0;
+}
+</style>
---
+++ Frontend Development/hands-on-10/src/components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+
+<header>
+
+<h1>Student Portal</h1>
+
+<p>
+
+Enrolled Courses :
+
+{{ enrolledCourses.length }}
+
+</p>
+
+</header>
+
+</template>
+
+<script setup>
+
+import { useEnrollmentStore } from "../stores/enrollment";
+
+import { storeToRefs } from "pinia";
+
+const store = useEnrollmentStore();
+
+const { enrolledCourses } = storeToRefs(store);
+
+</script>
+
+<style scoped>
+
+header{
+
+background:#1976d2;
+
+color:white;
+
+padding:20px;
+
+text-align:center;
+
+}
+
+</style>
