d5222732eebac211c28731963e69fd439cd460a1
KANISHA M D <kanishamd26@gmail.com>
2026-06-26 18:26:25 +0530

    Add files via upload

---
+++ Frontend Development/hands-on-8/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>Student Portal</h1>
+
+    <p>
+      Enrolled Courses: {{ store.enrolledCourses.length }}
+    </p>
+  </header>
+</template>
+
+<script setup>
+import { useEnrollmentStore } from '../stores/enrollment'
+
+const store = useEnrollmentStore()
+</script>
+
+<style scoped>
+header {
+  background: #42b883;
+  color: white;
+  padding: 15px;
+  text-align: center;
+}
+</style>
