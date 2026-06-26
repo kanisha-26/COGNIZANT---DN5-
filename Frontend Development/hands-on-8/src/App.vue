b7cc840a0f560ae51f06f705efa0290a1a97fa4a
KANISHA M D <kanishamd26@gmail.com>
2026-06-26 18:30:51 +0530

    Add files via upload

---
+++ Frontend Development/hands-on-8/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header />
+
+  <nav>
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/courses">Courses</RouterLink> |
+    <RouterLink to="/profile">Profile</RouterLink>
+  </nav>
+
+  <RouterView />
+</template>
+
+<script setup>
+import Header from './components/Header.vue'
+import { RouterLink, RouterView } from 'vue-router'
+</script>
