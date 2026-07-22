6094deefd490db8cca554d0e9ef75cc7c3012ed0
KANISHA M D <kanishamd26@gmail.com>
2026-07-22 21:55:53 +0530

    Add files via upload

---
+++ python Backend Frameworks/handson_3/courses/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    CourseViewSet,
+    StudentViewSet,
+    EnrollmentViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet)
+router.register(r'students', StudentViewSet)
+router.register(r'enrollments', EnrollmentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
